c7fe1f9975f65535c84ec168a45540b78ed6091c
HyeonJinCho <78816754+cho-hyeonjin@users.noreply.github.com>
2023-12-11 22:01:45 +0900

    ⚙️ setting: Webpack Entry, Output 설정 연습 & DevServer 적용 연습 (#4)
    * Revert "[feature] Eslint(Airbnb), Prettier Setting"
    
    This reverts commit c5cf21c9ae091c1a7904397eefe7765b400985ff.
    
    * practice: npm custom commands
    
    * practice: configuring Static Assets - html, js
    
    * practice: configuration for webpack bundling
    
    * practice: Entry Points Object Syntax
    
    * practice: webpack dev server setting test
    
    * practice: loader array
    
    * practice: loader configuration basic
    
    * practice: loader configuration - more detail setting
    
    * practice: plugin setting
    
    * practice: loader and plugin setting - babel
    
    * git: commit for git conflict solving
    
    * practice: add ts-loader for posting example
    
    * practice: webpack setting practice done

---
+++ webpack.config.js
@@ -0,0 +1,55 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+
+module.exports = {
+  mode: "none",
+  entry: {
+    main: "./src/main.js",
+    detail: "./src/detail.js",
+  },
+  output: {
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(?:js|mjs|cjs)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [["@babel/preset-env", { targets: "defaults" }]],
+          },
+        },
+      },
+      {
+        test: /\.css$/,
+        use: [
+          { loader: "style-loader" },
+          {
+            loader: "css-loader",
+            options: { modules: true },
+          },
+        ],
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          {
+            loader: "file-loader",
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "index.html",
+    }),
+  ],
+  devServer: {
+    port: 8000,
+    hot: true,
+  },
+};
