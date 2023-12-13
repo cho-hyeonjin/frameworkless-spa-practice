f40dd309f48293a843e9c9bbd4611cbd77e3e492
cho-hyeonjin <gusls2005@gmail.com>
2023-12-13 18:09:23 +0900

    practice: HTML outline for Todo MVC App - using temp late tags extensively

--- dist/main.bundle.js
+++ dist/main.bundle.js
@@ -7,16 +7,25 @@
 
 __webpack_require__.r(__webpack_exports__);
 /* harmony export */ __webpack_require__.d(__webpack_exports__, {
-/* harmony export */   "default": () => (/* binding */ GithubAvatar)
+/* harmony export */   EVENTS: () => (/* binding */ EVENTS),
+/* harmony export */   "default": () => (/* binding */ GitHubAvatar)
 /* harmony export */ });
 const ERROR_IMAGE = "https://files-82ee7vgzc.now.sh";
 const LOADING_IMAGE = "https://files-8bga2nnt0.now.sh";
-const getGithubAvatarUrl = async user => {
+// 이전 코드까지는 사용자와의 상호작용이 아닌 도메인에 국한된 DOM이벤트를 생성했다.
+// 이번 코드에서는 github avatar가 로드됐을 때와 오류가 발생했을 때 각각 다른 이벤트를 발생시켜보자!
+const AVATAR_LOAD_COMPLETE = "AVATAR_LOAD_COMPLETE"; // 성공한 경우,
+const AVATAR_LOAD_ERROR = "AVATAR_LOAD_ERROR"; // 실패한 경우를 분기 처리하여 렌더링하기 위한 식별자
+// 식별자들을 하나의 객체로 묶어 export 한다. (렌더링 엔진 모듈에서 DOM조작 이벤트 리스너에 parameter로 전달하기 위함)
+const EVENTS = {
+  AVATAR_LOAD_COMPLETE,
+  AVATAR_LOAD_ERROR
+};
+const getGitHubAvatarUrl = async user => {
   if (!user) {
     return;
   }
-  const url = `https://api.github.com/users/${user}`; // toss.tech 페이지 sources의 assets에 있는 이미지를 사용해보려 했으나, response가 json이 아닌 image 파일 자체여서 일단 예제코드 그대로 진행
-
+  const url = `https://api.github.com/users/${user}`;
   const response = await fetch(url);
   if (!response.ok) {
     throw new Error(response.statusText);
@@ -24,7 +33,7 @@ const getGithubAvatarUrl = async user => {
   const data = await response.json();
   return data.avatar_url;
 };
-class GithubAvatar extends HTMLElement {
+class GitHubAvatar extends HTMLElement {
   constructor() {
     super();
     this.url = LOADING_IMAGE;
@@ -43,6 +52,26 @@ class GithubAvatar extends HTMLElement {
       this.appendChild(img);
     });
   }
+
+  // HTTP 요청이 성공하여 github avatar가 로드 되었을 때 발생시키는 이벤트
+  onLoadAvatarComplete() {
+    const event = new CustomEvent(AVATAR_LOAD_COMPLETE, {
+      detail: {
+        avatar: this.url
+      }
+    });
+    this.dispatchEvent(event);
+  }
+
+  // HTTP 요청이 실패하여 github avatar가 로드 되지 못했을 때 발생시키는 이벤트
+  onLoadAvatarError(error) {
+    const event = new CustomEvent(AVATAR_LOAD_ERROR, {
+      detail: {
+        error
+      }
+    });
+    this.dispatchEvent(event);
+  }
   async loadNewAvatar() {
     const {
       user
@@ -51,9 +80,11 @@ class GithubAvatar extends HTMLElement {
       return;
     }
     try {
-      this.url = await getGithubAvatarUrl(user);
+      this.url = await getGitHubAvatarUrl(user);
+      this.onLoadAvatarComplete();
     } catch (e) {
       this.url = ERROR_IMAGE;
+      this.onLoadAvatarError(e);
     }
     this.render();
   }
@@ -127,6 +158,14 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var _components_GithubAvatar_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(1);
 
 window.customElements.define("github-avatar", _components_GithubAvatar_js__WEBPACK_IMPORTED_MODULE_0__["default"]);
+document.querySelectorAll("github-avatar").forEach(avatar => {
+  avatar.addEventListener(_components_GithubAvatar_js__WEBPACK_IMPORTED_MODULE_0__.EVENTS.AVATAR_LOAD_COMPLETE, e => {
+    console.log("Avatar Loaded", e.detail.avatar);
+  });
+  avatar.addEventListener(_components_GithubAvatar_js__WEBPACK_IMPORTED_MODULE_0__.EVENTS.AVATAR_LOAD_ERROR, e => {
+    console.log("Avatar Loading error", e.detail.error);
+  });
+});
 })();
 
 /******/ })()
