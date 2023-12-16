5d9ccfdf1f06f8e6d47f64bb5838176973ac914f
cho-hyeonjin <gusls2005@gmail.com>
2023-12-17 02:58:34 +0900

    MVC: Controller Preview

--- src/main.js
+++ src/main.js
@@ -1,17 +1,72 @@
-// Controller 컴포넌트
+// TodoMVC Controller - preview
 
-import createPages from "./pages.js";
-import createRouter from "./router.js";
+import todosView from "./view/todos.js";
+import counterView from "./view/counter.js";
+import filtersView from "./view/filters.js";
+import appView from "./view/app.js";
+import applyDiff from "./applyDiff.js";
 
-const container = document.querySelector("main");
-const pages = createPages(container);
-const router = createRouter();
+import registry from "./registry.js";
 
-router
-  .addRoute("/", pages.tech)
-  .addRoute("/design", pages.design)
-  .addRoute("/career", pages.career)
-  .addRoute("/list/:id", pages.detail)
-  .addRoute("/list/:id/:anotherId", pages.anotherDetail)
-  .setNotFound(pages.notFound)
-  .start();
+registry.add("app", appView);
+registry.add("todos", todosView);
+registry.add("counter", counterView);
+registry.add("filters", filtersView);
+
+const state = {
+  todos: [],
+  currentFilter: "All",
+};
+
+const events = {
+  addItem: (text) => {
+    state.todos.push({
+      text,
+      completed: false,
+    });
+    render();
+  },
+
+  updateItem: (index, text) => {
+    state.todos[index].text = text;
+    render();
+  },
+
+  deleteItem: (index) => {
+    state.todos.splice(index, 1);
+    render();
+  },
+
+  toggleItemCompleted: (index) => {
+    const { completed } = state.todos[index];
+    state.todos[index].completed = !completed;
+    render();
+  },
+
+  completeAll: () => {
+    state.todos.forEach((t) => {
+      t.completed = true;
+    });
+    render();
+  },
+
+  clearCompleted: () => {
+    state.todos = state.todos.filter((t) => !t.completed);
+    render();
+  },
+
+  changeFilter: (filter) => {
+    state.currentFilter = filter;
+    render();
+  },
+};
+
+const render = () => {
+  window.requestAnimationFrame(() => {
+    const main = document.querySelector("#root");
+    const newMain = registry.renderRoot(main, state, events);
+    applyDiff(document.body, main, newMain);
+  });
+};
+
+render();
