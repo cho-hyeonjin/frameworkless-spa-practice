1cbad185ee99ae521e964f1a845d7e9997260833
cho-hyeonjin <gusls2005@gmail.com>
2023-12-18 00:57:29 +0900

    MVC: Controller using Observarbla Model (main.js)

--- src/main.js
+++ src/main.js
@@ -1,53 +1,21 @@
-// Controller with Separated Model
-// 상태를 컨트롤러에서 유지하는 것은 상태 관리의 좋은 방법이 아닙니다.
-// 애플리케이션의 디자인(구조)을 향상시키는 첫 번째 단계는?
-// → 상태 코드를 별도의 파일로 옮겨서 분리하는 것!
-// 아래 코드는 애플리케이션의 상태를 외부 모델(분리된 별도의 상태 파일)에서 관리하는 컨트롤러의 업데이트된 버전입니다.
+// MVC -  Controller using Obserbarble Model
+// 옵저버블 패턴에서의 모델 객체에서 state를 get하는 방법은 리스너 콜백을 사용하는 것이었고,
+// 이 콜백은 구독할 때, 내부 상태가 변경될 때마다 호출되는데,
+// 이는 컨트롤러를 단순화시킵니다.
 
 // madel 객체(함수, model.js)를 modelFactory라는 이름으로 import
 import modelFactory from "./model/model.js";
 
 const model = modelFactory();
 
-const events = {
-  addItem: (text) => {
-    model.addItem(text);
-    render(model.getState());
-  },
-  updateItem: (index, text) => {
-    model.updateItem(index, text);
-    render(model.getState());
-  },
-  deleteItem: (index) => {
-    model.deleteItem(index);
-    render(model.getState());
-  },
-  toggleItemCompleted: (index) => {
-    model.toggleItemCompleted(index);
-    render(model.getState());
-  },
-  completeAll: () => {
-    model.completeAll();
-    render(model.getState());
-  },
-  clearCompleted: () => {
-    model.clearCompleted();
-    render(model.getState());
-  },
-  changeFilter: (filter) => {
-    model.changeFilter(filter);
-    render(model.getState());
-  },
-};
+const { addChangeListener, ...events } = model;
 
 const render = (state) => {
   window.requestAnimationFrame(() => {
     const main = document.querySelector("#root");
-
     const newMain = registry.renderRoot(main, state, events);
-
     applyDiff(document.body, main, newMain);
   });
 };
 
-render(model.getState()); // rendering에 사용된 실제 데이터는 model 객체의 getState 메서드에서 반환됩니다.
+addChangeListener(render);
