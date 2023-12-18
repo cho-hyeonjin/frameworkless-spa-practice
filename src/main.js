3be4d3e8f02fe6d13b517147fb15c1f2d93f0e84
cho-hyeonjin <gusls2005@gmail.com>
2023-12-18 17:33:50 +0900

    MVC: Observer Pattern TodoMVC Application

--- src/main.js
+++ src/main.js
@@ -1,10 +1,17 @@
-// MVC -  Controller using Obserbarble Model
-// 옵저버블 패턴에서의 모델 객체에서 state를 get하는 방법은 리스너 콜백을 사용하는 것이었고,
-// 이 콜백은 구독할 때, 내부 상태가 변경될 때마다 호출되는데,
-// 이는 컨트롤러를 단순화시킵니다.
+import todosView from "./view/todos.js";
+import counterView from "./view/counter.js";
+import filtersView from "./view/filters.js";
+import appView from "./view/app.js";
+import applyDiff from "./applyDiff.js";
 
-// madel 객체(함수, model.js)를 modelFactory라는 이름으로 import
+import registry from "./registry.js";
+
 import modelFactory from "./model/model.js";
+
+registry.add("app", appView);
+registry.add("todos", todosView);
+registry.add("counter", counterView);
+registry.add("filters", filtersView);
 
 const model = modelFactory();
 
@@ -13,7 +20,9 @@ const { addChangeListener, ...events } = model;
 const render = (state) => {
   window.requestAnimationFrame(() => {
     const main = document.querySelector("#root");
+
     const newMain = registry.renderRoot(main, state, events);
+
     applyDiff(document.body, main, newMain);
   });
 };
