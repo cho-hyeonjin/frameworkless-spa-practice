018d0114cd53d48da693085df669c0ecfdae134a
HyeonJinCho <78816754+cho-hyeonjin@users.noreply.github.com>
2023-12-23 16:49:20 +0900

    🏗️ practice: Web Component Ver.1 정적 렌더링 (#7)
    * Revert "[feature] Eslint(Airbnb), Prettier Setting"
    
    This reverts commit c5cf21c9ae091c1a7904397eefe7765b400985ff.
    
    * practice: npm custom commands
    
    * practice: configuring Static Assets - html, js
    
    * practice: configuration for webpack bundling
    
    * practice: Entry Points Object Syntax
    
    * practice: webpack dev server setting test
    
    * practice: loader array
    
    * practice: loader configuration basic
    
    * practice: loader configuration - more detail setting
    
    * practice: plugin setting
    
    * practice: loader and plugin setting - babel
    
    * git: commit for git conflict solving
    
    * practice: add ts-loader for posting example
    
    * practice: webpack setting practice done
    
    * practice: Web Components - Custom Elements (Basic of Basic)
    
    * practice: Web Components - Custom Elements - managing Attributes (get/set)
    
    * practice: Web Components - Custom Elements - managing Attributes (get/set) - add comments
    
    * practice: Web Component - custom element - attributeChangedCallback(after init render)
    
    * practice: Web Component - Virtual DOM Integration

--- dist/main.bundle.js
+++ dist/main.bundle.js
@@ -1,4 +1,162 @@
 /******/ (() => { // webpackBootstrap
+
+/******/ 	"use strict";
+/******/ 	var __webpack_modules__ = ([
+/* 0 */,
+/* 1 */
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (/* binding */ TestComponent)
+/* harmony export */ });
+/* harmony import */ var _applyDiff__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(2);
+
+const DEFAULT_COLOR = "black";
+const createDomElement = color => {
+  const div = document.createElement("div");
+  div.textContent = "createDomElement🪄";
+  div.style.color = color;
+  return div;
+};
+class TestComponent extends HTMLElement {
+  static get observedAttributes() {
+    return ["color"];
+  }
+  get color() {
+    return this.getAttribute("color") || DEFAULT_COLOR;
+  }
+  set color(value) {
+    this.setAttribute("color", value);
+  }
+  attributeChangedCallback(name, oldValue, newValue) {
+    if (!this.hasChildNodes()) {
+      return;
+    }
+    (0,_applyDiff__WEBPACK_IMPORTED_MODULE_0__["default"])(this,
+    // parentNode
+    this.firstElementChild,
+    // realNode
+    createDomElement(newValue) // virtualNode
+    );
+  }
+  connectedCallback() {
+    window.requestAnimationFrame(() => {
+      this.appendChild(createDomElement(this.color));
+    });
+  }
+}
+
+/***/ }),
+/* 2 */
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+const isNodeChanged = (node1, node2) => {
+  const n1Attributes = node1.attributes;
+  const n2Attributes = node2.attributes;
+  if (n1Attributes.length !== n2Attributes.length) {
+    return true;
+  }
+  const differentAttribute = Array.from(n1Attributes).find(attribute => {
+    const {
+      name
+    } = attribute;
+    const attribute1 = node1.getAttribute(name);
+    const attribute2 = node2.getAttribute(name);
+    return attribute1 !== attribute2;
+  });
+  if (differentAttribute) {
+    return true;
+  }
+  if (node1.children.length === 0 && node2.children.length === 0 && node1.textContent !== node2.textContent) {
+    return true;
+  }
+  return false;
+};
+const applyDiff = (parentNode, realNode, virtualNode) => {
+  if (realNode && !virtualNode) {
+    realNode.remove();
+    return;
+  }
+  if (!realNode && virtualNode) {
+    parentNode.appendChild(virtualNode);
+    return;
+  }
+  if (isNodeChanged(virtualNode, realNode)) {
+    realNode.replaceWith(virtualNode);
+    return;
+  }
+  const realChildren = Array.from(realNode.children);
+  const virtualChildren = Array.from(virtualNode.children);
+  const max = Math.max(realChildren.length, virtualChildren.length);
+  for (let i = 0; i < max; i++) {
+    applyDiff(realNode, realChildren[i], virtualChildren[i]);
+  }
+};
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (applyDiff);
+
+/***/ })
+/******/ 	]);
+/************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/define property getters */
+/******/ 	(() => {
+/******/ 		// define getter functions for harmony exports
+/******/ 		__webpack_require__.d = (exports, definition) => {
+/******/ 			for(var key in definition) {
+/******/ 				if(__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
+/******/ 					Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
+/******/ 				}
+/******/ 			}
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
+/******/ 	(() => {
+/******/ 		__webpack_require__.o = (obj, prop) => (Object.prototype.hasOwnProperty.call(obj, prop))
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/make namespace object */
+/******/ 	(() => {
+/******/ 		// define __esModule on exports
+/******/ 		__webpack_require__.r = (exports) => {
+/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 			}
+/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/************************************************************************/
+
 var __webpack_exports__ = {};
 function initMain() {
   const element = document.createElement("h1");
--- src/main.js
+++ src/main.js
@@ -1,6 +1,22 @@
+// 이 컴포넌트는 main 페이지 rendering 엔진 역할을 하는 컴포넌트로 생각하고 작성중
+import TestComponent from "./components/TestComponent";
+
+window.customElements.define("test-component", TestComponent);
+
+const changeColorTo = (color) => {
+  document.querySelectorAll("test-component").forEach((testComponent) => {
+    testComponent.color = color;
+  });
+};
+
+document.querySelector("button").addEventListener("click", () => {
+  changeColorTo("blue");
+});
+
 function initMain() {
   const element = document.createElement("h1");
   element.innerHTML = "메인 컴포넌트";
   return element;
 }
 document.body.appendChild(initMain());
+
